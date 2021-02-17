1bc7459a94f795eabb38eb8411b78ee0d595b84b
Kyle Poole <poolekyle@gmail.com>
2021-02-16 17:18:25 -0800

    Extra refinements - start screen, better collisions, etc.

--- BaseEnemy.gd
+++ BaseEnemy.gd
@@ -7,8 +7,12 @@ signal enemy_has_collided_with_player
 
 onready var sprite: Sprite = $EnemySprite
 
+func _process(_delta):
+	if position.x < 800:
+		$EnemyCollisionShape.disabled = false
+
 func on_enemy_collision(body):
-	if position.x > 800 and "EnemyBody" in body.name:
+	if position.x > 800 and ("EnemyBody" in body.name or "AsteroidBody" in body.name):
 		return
 	
 	if has_been_hit == false and !("AsteroidBody" in body.name):
@@ -20,11 +24,11 @@ func on_enemy_collision(body):
 		$ExplosionSprite.visible = true
 		$ExplosionSprite.play("boom")
 		$ExplosionSprite.playing = true
-		# $EnemyCollisionShape.disabled = true
+		set_deferred("$EnemyCollisionShape.disabled", true)
 		$EnemySprite.visible = false
 
 func _on_EnemyBody_body_entered(body):
-	if "PlayerBody" in body.name and !$ExplosionSprite.visible:
+	if "PlayerBody" in body.name:
 		call_deferred("emit_signal", "enemy_has_collided_with_player", body)
 	if "BulletBody" in body.name:
 		call_deferred("emit_signal", "bullet_has_collided_with_enemy", body)
--- Game.gd
+++ Game.gd
@@ -10,14 +10,16 @@ var Asteroid = preload("res://Asteroid.tscn")
 var Bullet = preload("res://Bullet.tscn")
 
 var background_updates = 0
-var end_score = 0
 var player = null
+var game_over_delay = false
 
 func _ready():
 	randomize()
 	
 func _process(_delta):
 	if is_instance_valid(player) != true:
+		if game_over_delay == true:
+			return
 		if Input.is_action_pressed("ui_accept"):
 			start_game()
 		return
@@ -32,7 +34,17 @@ func start_game():
 	$TitleTextNode.visible = false
 	$GameOverTextNode.visible = false
 	
-	yield(get_tree().create_timer(1), "timeout")
+	var wait_time
+	if fetch_high_score() == 0:
+		wait_time = 5
+	else:
+		wait_time = 2
+	
+	$ReadyTimer.start()
+	$GetReadyTextNode.visible = true
+	yield(get_tree().create_timer(wait_time), "timeout")
+	$GetReadyTextNode.visible = false
+	$ReadyTimer.stop()
 	
 	player = Player.instance()
 	add_child(player)
@@ -52,17 +64,37 @@ func cleanup_previous_game():
 			child.queue_free()
 
 func on_game_over():
-	end_score = player.score
+	var high_score = update_high_score(player.score)
 	$EnemyTimer.stop()
 	$SmarterEnemyTimer.stop()
 	$AsteroidTimer.stop()
 	$ScoreTimer.stop()
-	$GameOverTextNode/GameOverText.text = "GAME OVER\nSCORE: " + str(end_score) + "\n\nTRY AGAIN?" 
+	$GameOverTextNode/GameOverText.text = "GAME OVER\nSCORE: " + str(player.score) + "\nRECORD: " + str(high_score) + "\nSHOOT TO RETRY" 
 	$HudNode/Score.bbcode_text = "[right]SCORE: 0[/right]"
 	$GameOverTextNode.visible = true
 	$HudNode.visible = false
+	game_over_delay = true
+	$GameOverTimer.start()
 	cleanup_previous_game()
+	
+func fetch_high_score():
+	var file_handler = File.new()
+	file_handler.open("high_score", File.READ)
+	var value = file_handler.get_64()
+	file_handler.close()
+	return value
 
+func update_high_score(score):
+	var previous_high_score = fetch_high_score()
+	if score <= previous_high_score:
+		return previous_high_score
+	
+	var file_handler = File.new()
+	file_handler.open("high_score", File.WRITE)
+	file_handler.store_64(score)
+	file_handler.close()
+	return score
+	
 func _on_bullet_has_collided_with_enemy(body):
 	if is_instance_valid(player) != true:
 		return
@@ -107,7 +139,7 @@ func _on_EnemyTimer_timeout():
 	new_enemy.add_to_group("instance")
 	new_enemy.connect("bullet_has_collided_with_enemy", self, "_on_bullet_has_collided_with_enemy")
 	new_enemy.connect("enemy_has_collided_with_player", self, "_on_object_has_collided_with_player")
-	new_enemy.global_position = Vector2(SCREEN_WIDTH, rand_range(10, 500))
+	new_enemy.global_position = Vector2(SCREEN_WIDTH, rand_range(-25, 375))
 	new_enemy.linear_velocity = Vector2(-500, 0)
 
 func _on_SmarterEnemyTimer_timeout():
@@ -116,7 +148,7 @@ func _on_SmarterEnemyTimer_timeout():
 	new_enemy.add_to_group("instance")
 	new_enemy.connect("bullet_has_collided_with_enemy", self, "_on_bullet_has_collided_with_enemy")
 	new_enemy.connect("enemy_has_collided_with_player", self, "_on_object_has_collided_with_player")
-	new_enemy.global_position = Vector2(SCREEN_WIDTH, rand_range(10, 500))
+	new_enemy.global_position = Vector2(SCREEN_WIDTH, rand_range(0, 425))
 	new_enemy.path = $Navigation2D.get_simple_path(player.position, new_enemy.position)
 
 func _on_AsteroidTimer_timeout():
@@ -126,7 +158,7 @@ func _on_AsteroidTimer_timeout():
 	new_asteroid.connect("asteroid_has_collided_with_player", self, "_on_object_has_collided_with_player")
 	new_asteroid.connect("asteroid_has_collided_with_enemy", self, "_on_asteroid_has_collided_with_enemy")
 	new_asteroid.connect("asteroid_has_collided_with_bullet", self, "_on_asteroid_has_collided_with_bullet")
-	new_asteroid.global_position = Vector2(SCREEN_WIDTH, rand_range(10, 500))
+	new_asteroid.global_position = Vector2(SCREEN_WIDTH, rand_range(0, 500))
 	new_asteroid.linear_velocity = Vector2(-200, 0)
 
 func _on_BackgroundTimer_timeout():
@@ -139,7 +171,7 @@ func _on_BackgroundTimer_timeout():
 		$ParallaxBackground/ParallaxLayer/TextureRect.set_position(Vector2(current_pos.x - 1, current_pos.y))
 
 func _on_TitleText_gui_input(event):
-	if event is InputEventMouseButton:
+	if event is InputEventMouseButton and game_over_delay == false:
 		start_game()
 
 func _on_ScoreTimer_timeout():
@@ -161,3 +193,9 @@ func _on_PlanetNode_body_entered(body):
 		body.on_enemy_collision(body)
 	if "PlayerBody" in body.name:
 		_on_object_has_collided_with_player(body)
+
+func _on_ReadyTimer_timeout():
+	$GetReadyTextNode/GetReadyText.visible = !$GetReadyTextNode/GetReadyText.visible
+
+func _on_GameOverTimer_timeout():
+	game_over_delay = false
